4c27d386bdaa97babc59f999b26029dd8f015354
zorianafoshchan <zoryana0403@gmail.com>
2023-02-01 20:38:16 +0200

    base__page-100% register-100% login-100% error-100%

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+
+
 $(function(){
  $('.top-slider_inner').slick({
   dots: true,
@@ -13,4 +15,48 @@ $(function(){
   ratedFill: "#ffc35b",
   readOnly: true,
  });
+
+ function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+  return {
+   total,
+   days,
+   hours,
+   minutes,
+   seconds
+  };
+ }
+
+ function initializeClock(id, endtime) {
+  const clock = document.querySelector('.promo_clock');
+  const daysSpan = clock.querySelector('.promo_days');
+  const hoursSpan = clock.querySelector('.promo_hours');
+  const minutesSpan = clock.querySelector('.promo_minutes');
+  const secondsSpan = clock.querySelector('.promo_seconds');
+
+  function updateClock() {
+   const t = getTimeRemaining(endtime);
+
+   daysSpan.innerHTML = t.days;
+   hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+   minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+   secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+   if (t.total <= 0) {
+    clearInterval(timeinterval);
+   }
+  }
+
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
+ }
+
+ const deadline = $('.promo_clock').attr('data-time');
+ initializeClock('promo_clock', deadline);
 });
+
